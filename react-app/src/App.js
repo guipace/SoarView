44b280a2f070494f1fe212016f9f40e06db1204d
Guilherme Pace <pace.gui@gmail.com>
2021-03-05 11:58:46 -0600

    work on navbar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import LoginForm from "./components/auth/LoginForm";
-// import SignUpForm from "./components/auth/SignUpForm";
 import NavBar from "./components/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
