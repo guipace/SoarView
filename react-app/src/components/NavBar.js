91aa5b32a408b5ab2c2301629da725390fc67ddd
Guilherme Pace <pace.gui@gmail.com>
2021-03-05 12:52:54 -0600

    complete navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -11,37 +11,28 @@ const NavBar = ({ setAuthenticated }) => {
   return (
     sessionUser && (
       <>
-      <nav className='flex items-center shadow bg-primary h-20 '>
-        <NavLink className='hover:bg-secondary h-full' to="/" exact={true} activeClassName="active">
-          <img className='h-16 mx-5 ' src='https://soarview.s3.amazonaws.com/logo_text.png' alt='logo' />
+      <nav className='flex items-center shadow-lg bg-primary h-20 font-noto text-accent font-bold uppercase'>
+        <NavLink className='hover:bg-secondary h-full flex items-center' to="/" exact={true} activeClassName="active">
+          <img className='h-16 mx-5' src='https://soarview.s3.amazonaws.com/logo_text.png' alt='logo' />
         </NavLink>
         <div className='flex-grow'></div>
-        <div className='flex'>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Upload Flight
+        <div className='flex items-center h-full'>
+          <NavLink className='hover:no-underline hover:bg-secondary h-full flex items-center text-center tracking-widest' to="/" exact={true} activeClassName="active">
+            Upload
+            <br />
+            Flight
           </NavLink>
-          <div className='cursor-pointer' onClick={() => setDropdownVisible(prev => !prev)}>
-            <img className='h-16 mx-5 rounded-full bg-secondary' src={sessionUser.image_url} alt='user avatar' />
+          <div className='cursor-pointer' >
+            <img className='h-16 mx-5 rounded-full bg-secondary cursor-pointer transform hover:scale-105 hover:shadow' onClick={() => setDropdownVisible(prev => !prev)} src={sessionUser.image_url} alt='user avatar' />
           </div>
         </div>
-
-
-
-        {/* <ul>
-            <NavLink to="/sign-up" exact={true} activeClassName="active">
-              Sign Up
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/users" exact={true} activeClassName="active">
-              Users
-            </NavLink>
-          </li>
-        </ul> */}
       </nav>
       {dropdownVisible &&
-        <div className='bg-primary w-24 fixed right-0	'>
-          <LogoutButton setAuthenticated={setAuthenticated} />
+        <div className='bg-primary w-32 fixed right-0 flex flex-col' onMouseLeave={() => setDropdownVisible(prev => !prev)}>
+          <div className='text-center	py-2 hover:bg-secondary'>My Profile</div>
+          <div className='hover:bg-secondary'>
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </div>
         </div>}
       </>
     )
