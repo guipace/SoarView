d82e8f20482b02b997f1a166406908ea1ee2a25b
Guilherme Pace <pace.gui@gmail.com>
2021-03-05 11:58:46 -0600

    work on navbar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = ({ setAuthenticated }) => {
+  const sessionUser = useSelector(state => state.session.user)
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
   return (
-    <nav>
-      <ul>
-        <li>
+    sessionUser && (
+      <>
+      <nav className='flex items-center shadow bg-primary h-20 '>
+        <NavLink className='hover:bg-secondary h-full' to="/" exact={true} activeClassName="active">
+          <img className='h-16 mx-5 ' src='https://soarview.s3.amazonaws.com/logo_text.png' alt='logo' />
+        </NavLink>
+        <div className='flex-grow'></div>
+        <div className='flex'>
           <NavLink to="/" exact={true} activeClassName="active">
-            Home
+            Upload Flight
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
+          <div className='cursor-pointer' onClick={() => setDropdownVisible(prev => !prev)}>
+            <img className='h-16 mx-5 rounded-full bg-secondary' src={sessionUser.image_url} alt='user avatar' />
+          </div>
+        </div>
+
+
+
+        {/* <ul>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/users" exact={true} activeClassName="active">
+              Users
+            </NavLink>
+          </li>
+        </ul> */}
+      </nav>
+      {dropdownVisible &&
+        <div className='bg-primary w-24 fixed right-0	'>
           <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
-    </nav>
+        </div>}
+      </>
+    )
   );
 }
 
