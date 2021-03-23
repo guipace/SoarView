abd7493a77a9e76f5b35a9f693c1bb49393741e0
Guilherme Pace <pace.gui@gmail.com>
2021-03-23 15:09:39 -0500

    improve home page background

--- react-app/src/components/pages/HomePage.js
+++ react-app/src/components/pages/HomePage.js
@@ -28,47 +28,49 @@ function HomePage() {
   }, [dispatch, startDate, endDate])
 
   return ( recentFlights &&
-    <div className='flex-1 mt-20 p-3 flex divide-x-2 font-noto'>
-      <div className='w-1/2 flex flex-col items-center'>
-        <div className='flex flex-col items-center'>
-          <h2 className='font-fira'>Most Recent Flights</h2>
+    <div className='flex-1 mt-20 bg-background1 bg-cover bg-center font-noto'>
+      <div className='p-3 flex divide-x-2 bg-background h-full md:w-max mx-auto'>
+        <div className='w-1/2 px-7 flex flex-col items-center'>
+          <div className='flex flex-col items-center'>
+            <h2 className='font-fira'>Most Recent Flights</h2>
+            <div>
+              {recentFlights.map(flight =>
+                <FlightCard key={flight.id} flight={flight} />
+              )}
+            </div>
+          </div>
+          {/* <div>
+            <h2>Top Pilots</h2>
+            <div>Containing results</div>
+          </div> */}
+        </div>
+        <div className='w-1/2 px-7 flex flex-col items-center'>
+          <div className='flex items-center bg-secondary p-5 rounded'>
+            <h2 className='font-fira'>Search</h2>
+            <div className='flex flex-col items-center mx-3'>
+              <label>Start Date</label>
+              <input
+                type='date'
+                value={startDate}
+                onChange={e => setStartDate(e.target.value)}
+              />
+            </div>
+            <div className='flex flex-col items-center'>
+              <label>End Date</label>
+              <input
+                type='date'
+                value={endDate}
+                onChange={e => setEndDate(e.target.value)}
+              />
+            </div>
+          </div>
+          { searchFlights &&
           <div>
-            {recentFlights.map(flight =>
+            {searchFlights.map(flight =>
               <FlightCard key={flight.id} flight={flight} />
             )}
-          </div>
+          </div>}
         </div>
-        {/* <div>
-          <h2>Top Pilots</h2>
-          <div>Containing results</div>
-        </div> */}
-      </div>
-      <div className='w-1/2 flex flex-col items-center'>
-        <div className='flex items-center bg-secondary p-5 rounded'>
-          <h2 className='font-fira'>Search</h2>
-          <div className='flex flex-col items-center mx-3'>
-            <label>Start Date</label>
-            <input
-              type='date'
-              value={startDate}
-              onChange={e => setStartDate(e.target.value)}
-            />
-          </div>
-          <div className='flex flex-col items-center'>
-            <label>End Date</label>
-            <input
-              type='date'
-              value={endDate}
-              onChange={e => setEndDate(e.target.value)}
-            />
-          </div>
-        </div>
-        { searchFlights &&
-        <div>
-          {searchFlights.map(flight =>
-            <FlightCard key={flight.id} flight={flight} />
-          )}
-        </div>}
       </div>
     </div>
   )
